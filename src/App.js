dba54cc8f752843285f43c9988e3fd1652693a79
Leonardo Lucas de Oliveira <Iruko09@gmail.com>
2023-07-14 13:29:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/Formulario.js';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Banner/>
+      <Formulario/>
+
+    </div>
+  );
+}
+
+export default App;
